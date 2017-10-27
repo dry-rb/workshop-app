deb5aa7a0dc4bd8e5a6155d555f413d724e23874
Tim Riley <tim@openmonkey.com>
2017-10-27 21:20:38 +1100

    Complete implementation of type, struct, and schema

--- apps/admin/lib/blog/admin/articles/form_schema.rb
+++ apps/admin/lib/blog/admin/articles/form_schema.rb
@@ -5,6 +5,9 @@ module Blog
   module Admin
     module Articles
       FormSchema = Dry::Validation.Form do
+        required(:title).filled
+        required(:status).filled(included_in?: Types::ArticleStatus.values)
+        required(:published_at).maybe(:time?)
       end
     end
   end
--- lib/blog/entities/article.rb
+++ lib/blog/entities/article.rb
@@ -3,6 +3,9 @@ require "types"
 module Blog
   module Entities
     class Article < Dry::Struct
+      attribute :title, Types::Strict::String
+      attribute :status, Types::ArticleStatus
+      attribute :published_at, Types::Strict::Time.optional
     end
   end
 end
