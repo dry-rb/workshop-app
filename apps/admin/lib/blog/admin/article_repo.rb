0a6d9aace354ad28d5c5636e536f8d31fe0f3770
Tim Riley <tim@openmonkey.com>
2017-10-28 15:35:44 +1100

    Add implementation of admin article repo

--- apps/admin/lib/blog/admin/article_repo.rb
+++ apps/admin/lib/blog/admin/article_repo.rb
@@ -1,8 +1,26 @@
 require "blog/repository"
+require_relative "entities"
 
 module Blog
   module Admin
     class ArticleRepo < Blog::Repository[:articles]
+      struct_namespace Entities
+
+      commands :create
+
+      def listing
+        articles.ordered_by_created_at
+      end
+
+      def by_id(id)
+        articles.by_pk(id).one
+      end
+
+      private
+
+      def articles
+        super.combine(:author)
+      end
     end
   end
 end
--- lib/persistence/relations/articles.rb
+++ lib/persistence/relations/articles.rb
@@ -1,7 +1,15 @@
 module Persistence
   module Relations
     class Articles < ROM::Relation[:sql]
-      schema :articles, infer: true
+      schema :articles, infer: true do
+        associations do
+          belongs_to :author
+        end
+      end
+
+      def ordered_by_created_at
+        order(self[:created_at].desc)
+      end
     end
   end
 end
